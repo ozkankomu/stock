e03e8c5af3b14ac3b2e2b2de1ccbe580ea083526
ozkankomu <ozkankomu35@gmail.com>
2022-11-28 13:09:41 +0300

    firms card added

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,14 +1,35 @@
+import { Button, Grid, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import FirmCard from "../components/FirmCard";
 import useStockCalls from "../hooks/useStockCalls";
 
 const Firms = () => {
   const { getFirms } = useStockCalls();
+  const { firms } = useSelector((state) => state.stock);
 
   useEffect(() => {
     getFirms();
   }, []);
 
-  return <div></div>;
+  return (
+    <Box>
+      <Typography variant="h4" color="error">
+        Firms
+      </Typography>
+      <Button variant="contained">New Firms</Button>
+      {firms?.length > 0 && (
+        <Grid container justifyContent="center" mt={3} gap={3}>
+          {firms?.map((firm) => (
+            <Grid item>
+              <FirmCard key={firm.id} firm={firm} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Box>
+  );
 };
 
 export default Firms;
