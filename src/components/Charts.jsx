393c7c053a67d564d992f2b6925923bbb2d66a80
ozkankomu <ozkankomu35@gmail.com>
2022-12-01 23:44:30 +0300

    sales chart added

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Card, Title, LineChart } from "@tremor/react";
+import { Grid, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
 
 const Charts = () => {
-  return <div>Charts</div>;
+  const { sales, purchases } = useSelector((state) => state.stock);
+  const dataFormatter = (number) =>
+    `${Intl.NumberFormat("us").format(number).toString()}%`;
+
+  const salesData = sales?.map((sale) => ({
+    date: sale.createds,
+    sales: sale.price_total,
+  }));
+  return (
+    <Grid container justifyContent="center">
+      <Grid item xs={12} sm={12} md={6}>
+        <Card>
+          <Typography>Daily Sales (USD)</Typography>
+          <LineChart
+            data={salesData}
+            dataKey="year"
+            categories={["Sales"]}
+            colors={["blue"]}
+            valueFormatter={dataFormatter}
+            marginTop="mt-6"
+            yAxisWidth="w-10"
+          />
+        </Card>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Charts;
--- src/components/KpiCards.jsx
+++ src/components/KpiCards.jsx
@@ -7,38 +7,46 @@ import PaymentsIcon from "@mui/icons-material/Payments";
 import { amber, indigo, pink } from "@mui/material/colors";
 import { flex } from "../styles/globalStyle";
 import { useSelector } from "react-redux";
-
-const data = [
-  {
-    title: "SALES",
-    metric: "$0",
-    icon: <MonetizationOnIcon />,
-    color: indigo[900],
-    bgColor: indigo[100],
-  },
-  {
-    title: "PROFIT",
-    metric: "$0",
-    icon: <PaymentsIcon />,
-    color: pink[900],
-    bgColor: pink[100],
-  },
-  {
-    title: "PURCHASES",
-    metric: "$0",
-    icon: <ShoppingCartIcon />,
-    color: amber[900],
-    bgColor: amber[100],
-  },
-];
+import { Card, Title, LineChart } from "@tremor/react";
 
 const KpiCards = () => {
   const { sales, purchases } = useSelector((state) => state?.stock);
+
+  const total = (data) =>
+    data
+      ?.map((item) => Number(item.price_total))
+      .reduce((acc, val) => acc + val, 0);
+
+  const totalProfit = total(sales) - total(purchases);
+
+  const data = [
+    {
+      title: "SALES",
+      metric: `$${total(sales) || 0}`,
+      icon: <MonetizationOnIcon sx={{ fontSize: "3rem" }} />,
+      color: indigo[900],
+      bgColor: indigo[100],
+    },
+    {
+      title: "PROFIT",
+      metric: `$${totalProfit || 0}`,
+      icon: <PaymentsIcon sx={{ fontSize: "3rem" }} />,
+      color: pink[900],
+      bgColor: pink[100],
+    },
+    {
+      title: "PURCHASES",
+      metric: `$${total(purchases) || 0}`,
+      icon: <ShoppingCartIcon sx={{ fontSize: "3rem" }} />,
+      color: amber[900],
+      bgColor: amber[100],
+    },
+  ];
   return (
     <div>
       <Grid container justifyContent="center" alignItems="center" spacing={2}>
         {data.map((item, index) => (
-          <Grid item key={index} xs={12} sm={6} md={4}>
+          <Grid item key={index} sx={{ width: "400px" }}>
             <Paper sx={{ p: 2 }} elevation={10}>
               <Box sx={flex}>
                 <Avatar
